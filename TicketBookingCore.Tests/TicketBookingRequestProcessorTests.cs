e9fa2871402fb24d16868991aaaf3eddecc15222
Mattis Ericsson Bergman <mattisodin@gmail.com>
2025-01-30 09:09:27 +0100

    Add project files.

---
+++ TicketBookingCore.Tests/TicketBookingRequestProcessorTests.cs
@@ -0,0 +1,42 @@
+namespace TicketBookingCore.Tests
+{
+    public class TicketBookingRequestProcessorTests
+    {
+        private readonly TicketBookingRequestProcessor _processor;
+        public TicketBookingRequestProcessorTests()
+        {
+            _processor = new TicketBookingRequestProcessor();
+        }
+
+        [Fact]
+        public void ShouldReturnTicketBookningResultWithRequestValues()
+        {
+            // Arrange
+
+            var request = new TicketBookingRequest
+            {
+                FirstName = "Mattis",
+                LastName = "Ericsson Bergman",
+                Email = "mattis@gmail.com"
+            };
+
+            // Act
+            TicketBookingResponse response = _processor.Book(request);
+
+            // Assert
+            Assert.NotNull(response);
+            Assert.Equal(request.FirstName, response.FirstName);
+            Assert.Equal(request.LastName, response.LastName);
+            Assert.Equal(request.Email, response.Email);
+        }
+        [Fact]
+        public void ShouldThrowExceptionIfRequestIsNull()
+        {
+            // Act
+            var exception = Assert.Throws<ArgumentNullException>(() => _processor.Book(null));
+
+            //Assert
+            Assert.Equal("request", exception.ParamName);
+        }
+    }
+}
